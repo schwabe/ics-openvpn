a92ab487086cff56272f9ec426631aec70f1fda4
Arne Schwabe <arne@rfc2549.org>
2012-05-23 16:47:47 +0200

    Finetune FAQ, change font sizes, add Information about broken Images from Sony and HTC

--- src/de/blinkt/openvpn/FaqFragment.java
+++ src/de/blinkt/openvpn/FaqFragment.java
@@ -2,9 +2,12 @@ package de.blinkt.openvpn;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class FaqFragment extends Fragment  {
 
@@ -18,6 +21,10 @@ public class FaqFragment extends Fragment  {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
     		Bundle savedInstanceState) {
     	View v= inflater.inflate(R.layout.faq, container, false);
+    	
+    	TextView bImages = (TextView) v.findViewById(R.id.brokenimages);
+    	bImages.setText(Html.fromHtml(getActivity().getString(R.string.broken_images_faq)));
+    	bImages.setMovementMethod(LinkMovementMethod.getInstance());
 		return v;
 
     }

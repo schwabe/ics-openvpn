578b0013f0f3627077d64596137ec074aae12259
Arne Schwabe <arne@rfc2549.org>
2012-07-17 21:07:39 +0200

    I really like the action bar, but not using the action bar is even better ;)

--- src/de/blinkt/openvpn/VPNPreferences.java
+++ src/de/blinkt/openvpn/VPNPreferences.java
@@ -2,13 +2,18 @@ package de.blinkt.openvpn;
 
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class VPNPreferences extends PreferenceActivity {
 
 	private String mProfileUUID;
+	private VpnProfile mProfile;
 
 	public VPNPreferences() {
 		super();
@@ -28,9 +33,9 @@ public class VPNPreferences extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		mProfileUUID = getIntent().getStringExtra(getPackageName() + ".profileUUID");
-		VpnProfile profile = ProfileManager.get(this,mProfileUUID);
-		if(profile!=null) {
-			setTitle(getString(R.string.edit_profile_title, profile.getName()));
+		mProfile = ProfileManager.get(this,mProfileUUID);
+		if(mProfile!=null) {
+			setTitle(getString(R.string.edit_profile_title, mProfile.getName()));
 		}
 		super.onCreate(savedInstanceState);
 	}
@@ -50,6 +55,44 @@ public class VPNPreferences extends PreferenceActivity {
 	public void onBackPressed() {
 		setResult(RESULT_OK, getIntent());
 		super.onBackPressed();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == R.id.remove_vpn)
+			askProfileRemoval();
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		getMenuInflater().inflate(R.menu.vpnpreferences_menu, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	private void askProfileRemoval() {
+		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+		dialog.setTitle("Confirm deletion");
+		dialog.setMessage(getString(R.string.remove_vpn_query, mProfile.mName));
+
+		dialog.setPositiveButton(android.R.string.yes,
+				new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				removeProfile(mProfile);
+			}
+
+		});
+		dialog.setNegativeButton(android.R.string.no,null);
+		dialog.create().show();
+	}
+
+	protected void removeProfile(VpnProfile profile) {
+		ProfileManager.getInstance(this).removeProfile(this,profile);
+		setResult(VPNProfileList.RESULT_VPN_DELETED);
+		finish();
 	}
 }
 

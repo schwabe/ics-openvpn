eb2da6d74b1e9c3982523451fce34f9d2ec6e2ff
Arne Schwabe <arne@rfc2549.org>
2012-07-21 01:12:21 +0200

    Change config.h version number to reflect openvpn version

--- openvpn/config.h
+++ openvpn/config.h
@@ -450,7 +450,7 @@
 #define PACKAGE_NAME "OpenVPN"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "OpenVPN 2.3_alpha1"
+#define PACKAGE_STRING "OpenVPN 2.3_alpha3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "openvpn"

196a0efe693b5f6c4bd79defc247adbbdff0f7e0
Arne Schwabe <arne@rfc2549.org>
2012-09-04 23:43:23 +0200

    Allow embedding of plcs12 files from the fileselect dialog

--- src/de/blinkt/openvpn/Settings_Basic.java
+++ src/de/blinkt/openvpn/Settings_Basic.java
@@ -126,7 +126,7 @@ public class Settings_Basic extends Fragment implements View.OnClickListener, On
 		addFileSelectLayout(mClientCert);
 		addFileSelectLayout(mClientKey);
 		addFileSelectLayout(mpkcs12);
-		mpkcs12.setNoline();
+		mpkcs12.setBase64Encode();
 		mCaCert.setShowClear();
 
 	

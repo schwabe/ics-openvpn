97e00b7491da0ad5e6377bbf7f3ae7561d544ce5
Arne Schwabe <arne@rfc2549.org>
2012-10-08 19:27:03 +0200

    Add generating to script.
    Difference between zh-CN and zh-TW

--- fetchtranslations.sh
+++ fetchtranslations.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
+
+if [ "$ICSCROWDAPIKEY" != "" ]
+then
+	fetch -1 -o - http://api.crowdin.net/api/project/ics-openvpn/export?key=$ICSCROWDAPIKEY
+fi
+
 fetch http://crowdin.net/download/project/ics-openvpn.zip
 
 langtoinclude="de cs ko et fr he"
@@ -7,4 +13,12 @@ langtoinclude="de cs ko et fr he"
 for lang in $langtoinclude
 do
     tar xfv ics-openvpn.zip /res/values-$lang/
+done
+
+# Chinese language require zh-CN and zh-TW
+
+for lang in zh-CN
+do
+	fetch http://crowdin.net/download/project/ics-openvpn/$lang.zip
+	tar -xv -C res/values-$lang/ --strip-components 3 -f $lang.zip
 done

dc0492044e85a70217382bbe8163f01289930c4f
Arne Schwabe <arne@rfc2549.org>
2012-12-14 13:32:06 +0100

    proto_remote fix

--- openvpn/src/openvpn/options.c
+++ openvpn/src/openvpn/options.c
@@ -2889,7 +2889,7 @@ options_string (const struct options *o,
   buf_printf (&out, ",dev-type %s", dev_type_string (o->dev, o->dev_type));
   buf_printf (&out, ",link-mtu %d", EXPANDED_SIZE (frame));
   buf_printf (&out, ",tun-mtu %d", PAYLOAD_SIZE (frame));
-  buf_printf (&out, ",proto %s", proto2ascii (proto_remote (o->ce.proto, remote),o->ce.af, true));
+  buf_printf (&out, ",proto %s",  proto_remote (o->ce.proto, remote));
   if (o->tun_ipv6)
     buf_printf (&out, ",tun-ipv6");
 

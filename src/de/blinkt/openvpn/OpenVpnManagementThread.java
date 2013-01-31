9e4585aec0fb4fed41dac0d2418a89946160a611
Arne Schwabe <arne@rfc2549.org>
2013-01-31 17:45:21 +0100

    Allow finer control of notification messages

--- src/de/blinkt/openvpn/OpenVpnManagementThread.java
+++ src/de/blinkt/openvpn/OpenVpnManagementThread.java
@@ -290,24 +290,12 @@ public class OpenVpnManagementThread implements Runnable {
 		mLastIn=in;
 		mLastOut=out;
 
-		String netstat = String.format("In: %8s, %8s/s  Out %8s, %8s/s",
-				humanReadableByteCount(in, false),
-				humanReadableByteCount(diffin, false),
-				humanReadableByteCount(out, false),
-				humanReadableByteCount(diffout, false));
-		OpenVPN.updateStateString("BYTECOUNT",netstat);
+		OpenVPN.updateByteCount(in,out,diffin, diffout);
 
 
 	}
 
-	// From: http://stackoverflow.com/questions/3758606/how-to-convert-byte-size-into-human-readable-format-in-java
-	public static String humanReadableByteCount(long bytes, boolean si) {
-		int unit = si ? 1000 : 1024;
-		if (bytes < unit) return bytes + " B";
-		int exp = (int) (Math.log(bytes) / Math.log(unit));
-		String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
-		return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
-	}
+
 
 	private void processNeedCommand(String argument) {
 		int p1 =argument.indexOf('\'');

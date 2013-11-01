1a49c83ad2fd3c98149e15c5605a2916069674ed
Arne Schwabe <arne@rfc2549.org>
2013-11-01 02:04:28 +0100

    Fix for windows build bat (untested by me)
    Closes issue #189
    Thanks to Florian Gattung <fgattung@gmail.com>

--- misc/build-native.bat
+++ misc/build-native.bat
@@ -1,14 +1,18 @@
+
 @echo on
-call ndk-build -j 8
+call ndk-build APP_API=all -j 8
 
 cd libs
 mkdir ..\assets
-
+mkdir ..\build\
 
 for /D %%f in (*) do (
 	copy %%f\minivpn ..\assets\minivpn.%%f
 	del %%f\libcrypto.so
 	del %%f\libssl.so
+
+	mkdir ..\build\native-libs\%%f\
+	copy %%f\*.so  ..\build\native-libs\%%f\
 )
 
 cd ..

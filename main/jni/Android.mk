ff572e939b1aaa5373bef8d2cd1ce31daaa13cf8
Arne Schwabe <arne@rfc2549.org>
2014-02-18 16:15:01 +0100

    make it easier to disable breakpad
    --HG--
    extra : rebase_source : c6832fdaac3421708edd4140762d12836f5053a2

--- main/jni/Android.mk
+++ main/jni/Android.mk
@@ -2,15 +2,20 @@
 JNI_DIR := $(call my-dir)
 
 #USE_POLAR=1
+USE_BREAKPAD=0
 
 include lzo/Android.mk
 include snappy/Android.mk
 
 include openssl/Android.mk
 
-ifneq ($(TARGET_ARCH),mips)
-WITH_BREAKPAD=1
-include google-breakpad/android/google_breakpad/Android.mk
+ifeq ($(USE_BREAKPAD),1)
+	ifneq ($(TARGET_ARCH),mips)
+	WITH_BREAKPAD=1
+	include google-breakpad/android/google_breakpad/Android.mk
+	else
+	WITH_BREAKPAD=0
+	endif
 else
 WITH_BREAKPAD=0
 endif

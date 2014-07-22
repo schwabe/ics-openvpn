d656e6e3c165c836a5d8c9a556f09240f4a5cd46
Arne Schwabe <arne@rfc2549.org>
2014-07-23 01:53:03 +0200

    Tune FAQ
    --HG--
    extra : rebase_source : 3e91216fe140928991fbdecab28b688d09089597

--- main/src/main/java/de/blinkt/openvpn/fragments/FaqFragment.java
+++ main/src/main/java/de/blinkt/openvpn/fragments/FaqFragment.java
@@ -1,6 +1,7 @@
 package de.blinkt.openvpn.fragments;
 
 import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
@@ -17,19 +18,26 @@ public class FaqFragment extends Fragment  {
     		Bundle savedInstanceState) {
     	View v= inflater.inflate(R.layout.faq, container, false);
     	
-    	insertHtmlEntry(v,R.id.broken_images_faq,R.string.broken_images_faq);
-    	insertHtmlEntry(v,R.id.faq_howto,R.string.faq_howto);
+    	insertHtmlEntry(v, R.id.broken_images_faq,R.string.broken_images_faq);
+    	insertHtmlEntry(v, R.id.faq_howto,R.string.faq_howto);
     	insertHtmlEntry(v, R.id.baterry_consumption, R.string.baterry_consumption);  
     	insertHtmlEntry(v, R.id.faq_tethering, R.string.faq_tethering);
         insertHtmlEntry(v, R.id.faq_vpndialog43, R.string.faq_vpndialog43);
         insertHtmlEntry(v, R.id.faq_system_dialog_xposed, R.string.faq_system_dialog_xposed);
+
+        /* I think the problem mentioned in there should not affect, 4.3+ */
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            v.findViewById(R.id.broken_images_faq).setVisibility(View.GONE);
+            v.findViewById(R.id.broken_images_faq_title).setVisibility(View.GONE);
+        }
+
 		return v;
     }
 
 	private void insertHtmlEntry (View v, int viewId, int stringId) {
-		TextView faqitem = (TextView) v.findViewById(viewId);
-    	faqitem.setText(Html.fromHtml(getActivity().getString(stringId)));
-    	faqitem.setMovementMethod(LinkMovementMethod.getInstance());
+		TextView faqItem = (TextView) v.findViewById(viewId);
+    	faqItem.setText(Html.fromHtml(getActivity().getString(stringId)));
+    	faqItem.setMovementMethod(LinkMovementMethod.getInstance());
 
 	}
 

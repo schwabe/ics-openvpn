e04efb987be2f5ac85291b3393b1d6525338fefc
Arne Schwabe <arne@rfc2549.org>
2014-08-06 00:00:45 +0200

    Resort VPN preferences add mss fix

---
+++ main/src/main/java/de/blinkt/openvpn/fragments/Settings_Behaviour.java
@@ -0,0 +1,77 @@
+package de.blinkt.openvpn.fragments;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+import de.blinkt.openvpn.R;
+
+public class Settings_Behaviour extends OpenVpnPreferencesFragment implements OnPreferenceChangeListener {
+	private CheckBoxPreference mPersistent;
+	private ListPreference mConnectretrymax;
+	private EditTextPreference mConnectretry;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.vpn_behaviour);
+		
+		mPersistent = (CheckBoxPreference) findPreference("usePersistTun");
+		mConnectretrymax = (ListPreference) findPreference("connectretrymax");
+		mConnectretry = (EditTextPreference) findPreference("connectretry");
+		
+		mConnectretrymax.setOnPreferenceChangeListener(this);
+		mConnectretrymax.setSummary("%s");
+		
+		mConnectretry.setOnPreferenceChangeListener(this);
+		
+		
+		loadSettings();
+
+	}
+	
+	protected void loadSettings() {
+		mPersistent.setChecked(mProfile.mPersistTun);
+		
+		mConnectretrymax.setValue(mProfile.mConnectRetryMax);
+		onPreferenceChange(mConnectretrymax, mProfile.mConnectRetryMax);
+				
+		mConnectretry.setText(mProfile.mConnectRetry);
+		onPreferenceChange(mConnectretry, mProfile.mConnectRetry);
+	}
+
+
+	protected void saveSettings() {
+		mProfile.mConnectRetryMax = mConnectretrymax.getValue();
+		mProfile.mPersistTun = mPersistent.isChecked();
+		mProfile.mConnectRetry = mConnectretry.getText();
+	}
+
+	
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+	     if (preference == mConnectretrymax) {
+			if(newValue==null) {
+				newValue="5";
+			}
+			mConnectretrymax.setDefaultValue(newValue);
+			
+			for(int i=0;i<mConnectretrymax.getEntryValues().length;i++){
+				if(mConnectretrymax.getEntryValues().equals(newValue))
+					mConnectretrymax.setSummary(mConnectretrymax.getEntries()[i]);
+			}
+			
+		} else if (preference == mConnectretry) {
+			if(newValue==null || newValue=="")
+				newValue="5";
+			mConnectretry.setSummary(String.format("%s s" , newValue));
+		}
+			
+		return true;
+	}
+
+}
--- main/src/main/java/de/blinkt/openvpn/fragments/Settings_Obscure.java
+++ main/src/main/java/de/blinkt/openvpn/fragments/Settings_Obscure.java
@@ -3,22 +3,22 @@ package de.blinkt.openvpn.fragments;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.EditTextPreference;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.widget.Toast;
+
 import de.blinkt.openvpn.R;
+import de.blinkt.openvpn.VpnProfile;
 
 public class Settings_Obscure extends OpenVpnPreferencesFragment implements OnPreferenceChangeListener {
 	private CheckBoxPreference mUseRandomHostName;
 	private CheckBoxPreference mUseFloat;
 	private CheckBoxPreference mUseCustomConfig;
 	private EditTextPreference mCustomConfig;
-	private ListPreference mLogverbosity;
-	private CheckBoxPreference mPersistent;
-	private ListPreference mConnectretrymax;
-	private EditTextPreference mConnectretry;
+    private EditTextPreference mMssFixValue;
+    private CheckBoxPreference mMssFixCheckBox;
 
-	@Override
+    @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Load the preferences from an XML resource
@@ -28,66 +28,63 @@ public class Settings_Obscure extends OpenVpnPreferencesFragment implements OnPr
 		mUseFloat = (CheckBoxPreference) findPreference("useFloat");
 		mUseCustomConfig = (CheckBoxPreference) findPreference("enableCustomOptions");
 		mCustomConfig = (EditTextPreference) findPreference("customOptions");
-		mPersistent = (CheckBoxPreference) findPreference("usePersistTun");
-		mConnectretrymax = (ListPreference) findPreference("connectretrymax");
-		mConnectretry = (EditTextPreference) findPreference("connectretry");
-		
-		mConnectretrymax.setOnPreferenceChangeListener(this);
-		mConnectretrymax.setSummary("%s");
-		
-		mConnectretry.setOnPreferenceChangeListener(this);
-		
+        mMssFixCheckBox = (CheckBoxPreference) findPreference("mssFix");
+        mMssFixValue = (EditTextPreference) findPreference("mssFixValue");
+        mMssFixValue.setOnPreferenceChangeListener(this);
 		
 		loadSettings();
 
 	}
-	
-	protected void loadSettings() {
-		mUseRandomHostName.setChecked(mProfile.mUseRandomHostname);
-		mUseFloat.setChecked(mProfile.mUseFloat);
-		mUseCustomConfig.setChecked(mProfile.mUseCustomConfig);
-		mCustomConfig.setText(mProfile.mCustomConfigOptions);
-		mPersistent.setChecked(mProfile.mPersistTun);
-		
-		mConnectretrymax.setValue(mProfile.mConnectRetryMax);
-		onPreferenceChange(mConnectretrymax, mProfile.mConnectRetryMax);
-				
-		mConnectretry.setText(mProfile.mConnectRetry);
-		onPreferenceChange(mConnectretry, mProfile.mConnectRetry);
-	}
 
+    protected void loadSettings() {
+        mUseRandomHostName.setChecked(mProfile.mUseRandomHostname);
+        mUseFloat.setChecked(mProfile.mUseFloat);
+        mUseCustomConfig.setChecked(mProfile.mUseCustomConfig);
+        mCustomConfig.setText(mProfile.mCustomConfigOptions);
 
-	protected void saveSettings() {
+        if (mProfile.mMssFix == 0) {
+            mMssFixValue.setText(String.valueOf(VpnProfile.DEFAULT_MSSFIX_SIZE));
+            mMssFixCheckBox.setChecked(false);
+            setMssSummary(VpnProfile.DEFAULT_MSSFIX_SIZE);
+        } else {
+            mMssFixValue.setText(String.valueOf(mProfile.mMssFix));
+            mMssFixCheckBox.setChecked(true);
+            setMssSummary(mProfile.mMssFix);
+        }
+
+    }
+
+    private void setMssSummary(int value) {
+        mMssFixValue.setSummary(String.format("Configured MSS value: %d", value));
+    }
+
+    protected void saveSettings() {
 		mProfile.mUseRandomHostname = mUseRandomHostName.isChecked();
 		mProfile.mUseFloat = mUseFloat.isChecked();
 		mProfile.mUseCustomConfig = mUseCustomConfig.isChecked();
 		mProfile.mCustomConfigOptions = mCustomConfig.getText();
-		mProfile.mConnectRetryMax = mConnectretrymax.getValue();
-		mProfile.mPersistTun = mPersistent.isChecked();
-		mProfile.mConnectRetry = mConnectretry.getText();
+        if (mMssFixCheckBox.isChecked())
+            mProfile.mMssFix=Integer.parseInt(mMssFixValue.getText());
+        else
+            mProfile.mMssFix=0;
+
 	}
 
 	
 	@Override
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
-	     if (preference == mConnectretrymax) {
-			if(newValue==null) {
-				newValue="5";
-			}
-			mConnectretrymax.setDefaultValue(newValue);
-			
-			for(int i=0;i<mConnectretrymax.getEntryValues().length;i++){
-				if(mConnectretrymax.getEntryValues().equals(newValue))
-					mConnectretrymax.setSummary(mConnectretrymax.getEntries()[i]);
-			}
-			
-		} else if (preference == mConnectretry) {
-			if(newValue==null || newValue=="")
-				newValue="5";
-			mConnectretry.setSummary(String.format("%s s" , newValue));
-		}
-			
-		return true;
+		if (preference.getKey().equals("mssFixValue"))
+            try {
+                int v = Integer.parseInt((String) newValue);
+                if (v < 0 || v > 9000)
+                    throw new NumberFormatException("mssfix value");
+                setMssSummary(v);
+
+            } catch(NumberFormatException e) {
+                Toast.makeText(getActivity(), R.string.mssfix_invalid_value, Toast.LENGTH_LONG).show();
+                return false;
+            }
+        return true;
 	}
 
 }

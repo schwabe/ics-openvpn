deb731bba806d2bd3e85a1e7ea5c570530caf059
Arne Schwabe <arne@rfc2549.org>
2014-08-12 10:24:37 +0200

    include commented out Acra support
    --HG--
    extra : rebase_source : f3396cea18633962ffa9f21d0537b9d4588a9bf4

--- main/src/main/java/de/blinkt/openvpn/core/ICSOpenVPNApplication.java
+++ main/src/main/java/de/blinkt/openvpn/core/ICSOpenVPNApplication.java
@@ -1,14 +1,35 @@
 package de.blinkt.openvpn.core;
-
 import android.app.Application;
 
-/**
- * Created by arne on 28.12.13.
- */
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+import de.blinkt.openvpn.BuildConfig;
+import de.blinkt.openvpn.R;
+import de.blinkt.openvpn.core.PRNGFixes;
+
+/*
+@ReportsCrashes(
+        formKey = "",
+        formUri = "http://reports.blinkt.de/report-icsopenvpn",
+        reportType = org.acra.sender.HttpSender.Type.JSON,
+        httpMethod = org.acra.sender.HttpSender.Method.PUT,
+        formUriBasicAuthLogin="report-icsopenvpn",
+        formUriBasicAuthPassword="Tohd4neiF9Ai!!!!111eleven",
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_toast_text
+)
+*/
 public class ICSOpenVPNApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         PRNGFixes.apply();
+
+        if (BuildConfig.DEBUG) {
+            //ACRA.init(this);
+        }
     }
+
 }

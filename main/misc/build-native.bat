e8e622e62fe704c04f402bf0149a955adbb3b3e5
Arne Schwabe <arne@rfc2549.org>
2014-10-18 01:43:13 +0200

    Prepare OpenVPN for Android for the sweetest Android yet

--- main/misc/build-native.bat
+++ main/misc/build-native.bat
@@ -1,22 +1,25 @@
 
 @echo on
-echo Currently broken, feel free to fix and send me a patch, see .sh file
+echo Currently broken, feel free to fix and send me a patch, see the build-native.sh file how native libraries are build on UNIX
 exit 1
 
-call ndk-build APP_API=all -j 8
+call ndk-build APP_ABI=x86_64 -j 8 USE_BREAKPAD=0
 
 
 cd libs
-mkdir ..\assets
-mkdir ..\build\
+mkdir ..\ovpnlibs
+mkdir ..\ovpnlibs\assets
 
 for /D %%f in (*) do (
-	copy %%f\minivpn ..\assets\minivpn.%%f
+	copy %%f\nopievpn ..\ovpnlibs\assets\nopievpn.%%f
+	copy %%f\pievpn ..\ovpnlibs\assets\pievpn.%%f
+
 	del %%f\libcrypto.so
 	del %%f\libssl.so
 
-	mkdir ..\build\native-libs\%%f\
-	copy %%f\*.so  ..\build\native-libs\%%f\
+    mkdir ..\ovpnlibs\jniLibs
+	mkdir ..\ovpnlibs\jniLibs\%%f\
+	copy %%f\*.so  ..\ovpnlibs\jniLibs\%%f\
 )
 
 cd ..

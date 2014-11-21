0db19daf7cbe0ea0c9075931d7a6c50d53f39488
Arne Schwabe <arne@rfc2549.org>
2014-11-21 14:40:36 +0100

    Do not include the packages in the message, this code does not actually work
    --HG--
    extra : rebase_source : 87ab9e0f99faf04e37662cd85d6e93a8721dfae3

--- main/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
+++ main/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
@@ -476,7 +476,6 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
         cfg += "dns: " + TextUtils.join("|", mDnslist);
         cfg += "domain: " + mDomain;
         cfg += "mtu: " + mMtu;
-        cfg += "disallowed: " + mProfile.mAllowedAppsVpnAreDisallowed + "|".join(mProfile.mAllowedAppsVpn);
         return cfg;
     }
 

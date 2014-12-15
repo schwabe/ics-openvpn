04710ae16c2b25c5f72724bf37cdb00185fa86bf
Arne Schwabe <arne@rfc2549.org>
2014-12-15 10:15:32 +0100

    Fix very stupid problem (KK != LP)
    --HG--
    extra : rebase_source : bae8146612680b2d64dcb0b3c53d8efe209a323b

--- main/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
+++ main/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
@@ -498,7 +498,7 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
 
         VpnStatus.logInfo(R.string.last_openvpn_tun_config);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && mProfile.mAllowLocalLAN)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && mProfile.mAllowLocalLAN)
         {
             allowAllAFFamilies(builder);
         }

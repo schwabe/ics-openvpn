1c21f98ba741f390269753b89ed9e18050f6d285
Arne Schwabe <arne@rfc2549.org>
2014-12-17 17:05:35 +0100

    Update/clarify copyright.

--- main/src/main/java/org/spongycastle/util/io/pem/PemWriter.java
+++ main/src/main/java/org/spongycastle/util/io/pem/PemWriter.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2012-2014 Arne Schwabe
+ * Distributed under the GNU GPL v2 with additional terms. For full terms see the file doc/LICENSE.txt
+ */
+
 package org.spongycastle.util.io.pem;
 
 import java.io.BufferedWriter;

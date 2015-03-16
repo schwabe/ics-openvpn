d2d034c75fcac2514e94237015c3eb2542c476dc
Arne Schwabe <arne@rfc2549.org>
2015-03-16 10:32:17 +0100

    Update OpenVPN version

--- main/openvpn/config-version.h
+++ main/openvpn/config-version.h
@@ -1,3 +1,2 @@
-#define CONFIGURE_GIT_REVISION "icsopenvpn_627-cff5e3e9c3ac08df"
-#define CONFIGURE_GIT_FLAGS "+"
-
+#define CONFIGURE_GIT_REVISION "icsopenvpn_629-09e2cd726861dc61"
+#define CONFIGURE_GIT_FLAGS ""

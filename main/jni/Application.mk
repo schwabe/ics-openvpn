844308bc5c0f6895abb54950c37b0907755ddaae
Arne Schwabe <arne@rfc2549.org>
2015-08-03 12:41:16 +0200

    new version (0.6.31) and link stlport statically (closes #375)

--- main/jni/Application.mk
+++ main/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_ABI := arm64-v8a armeabi armeabi-v7a mips  x86 x86_64
 APP_PLATFORM := android-14
 
-APP_STL:=stlport_shared
+APP_STL:=stlport_static
 #APP_STL:=gnustl_shared 
 
 #APP_OPTIM := release

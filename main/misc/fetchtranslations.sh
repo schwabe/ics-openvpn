23b373fe97aa04909823a84bd2c3a0643da64092
Arne Schwabe <arne@rfc2549.org>
2015-10-06 00:28:16 +0200

    Update translations, welcome Slovenian

--- main/misc/fetchtranslations.sh
+++ main/misc/fetchtranslations.sh
@@ -15,9 +15,9 @@ wget -q http://crowdin.net/download/project/ics-openvpn.zip
 # Chinese language require zh-CN and zh-TW
 
 typeset -A langhash
-langhash=(zh-CN zh-rCN zh-TW zh-rTW id-ID in ca-ES ca cs-CZ cs et-EE et ja-JP ja ko-KR ko sv-SE sv uk-UA uk vi-VN vi)
+langhash=(zh-CN zh-rCN zh-TW zh-rTW id-ID in ca-ES ca cs-CZ cs et-EE et ja-JP ja ko-KR ko sv-SE sv uk-UA uk vi-VN vi sl-SI sl)
 
-langtoinclude="de es fr hu it no nl pl pt ro ru tr"
+langtoinclude="de es fr hu it no nl pl pt ro ru tr sl"
 
 for lang in $langtoinclude ${(k)langhash}
 do

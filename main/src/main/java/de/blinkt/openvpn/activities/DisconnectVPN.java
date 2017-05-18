88b4bdf9ddbae7e689a0207e953d606ed3ab7461
Arne Schwabe <arne@rfc2549.org>
2017-05-18 19:08:59 +0200

    close #628

--- main/src/main/java/de/blinkt/openvpn/activities/DisconnectVPN.java
+++ main/src/main/java/de/blinkt/openvpn/activities/DisconnectVPN.java
@@ -64,8 +64,8 @@ public class DisconnectVPN extends Activity implements DialogInterface.OnClickLi
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.title_cancel);
         builder.setMessage(R.string.cancel_connection_query);
-        builder.setNegativeButton(android.R.string.no, this);
-        builder.setPositiveButton(android.R.string.yes, this);
+        builder.setNegativeButton(android.R.string.cancel, this);
+        builder.setPositiveButton(R.string.cancel_connection, this);
         builder.setNeutralButton(R.string.reconnect, this);
         builder.setOnCancelListener(this);
 

1d15b4a52ea226033494f14cdef253147b912a5a
Josh Soref <jsoref@users.noreply.github.com>
2020-01-31 12:50:03 -0500

    spelling: reasons

--- main/src/main/java/de/blinkt/openvpn/core/ProfileManager.java
+++ main/src/main/java/de/blinkt/openvpn/core/ProfileManager.java
@@ -113,7 +113,7 @@ public class ProfileManager {
         Editor editor = sharedprefs.edit();
         editor.putStringSet("vpnlist", profiles.keySet());
 
-        // For reasing I do not understand at all
+        // For reasons I do not understand at all
         // Android saves my prefs file only one time
         // if I remove the debug code below :(
         int counter = sharedprefs.getInt("counter", 0);

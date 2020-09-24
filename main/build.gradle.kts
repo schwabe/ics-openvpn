154fee81f6f51728506595a597419989925663e7
Arne Schwabe <arne@rfc2549.org>
2020-09-24 19:11:40 +0200

    Version 0.7.21

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     defaultConfig {
         minSdkVersion(14)
         targetSdkVersion(30)  //'Q'.toInt()
-        versionCode = 174
-        versionName = "0.7.20"
+        versionCode = 175
+        versionName = "0.7.21"
 
         externalNativeBuild {
             cmake {

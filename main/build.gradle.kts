bb28013c1e9549e3229de8ba66bf508f3253adc5
Arne Schwabe <arne@rfc2549.org>
2022-01-09 19:24:50 +0100

    Version 0.7.32

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     defaultConfig {
         minSdk = 16
         targetSdk = 31
-        versionCode = 186
-        versionName = "0.7.31"
+        versionCode = 187
+        versionName = "0.7.32"
 
         externalNativeBuild {
             cmake {

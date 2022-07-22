4528aba9bad3782cd57c91205799eaf2cccd1e39
Arne Schwabe <arne@rfc2549.org>
2022-07-22 13:14:55 +0200

    Update gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }

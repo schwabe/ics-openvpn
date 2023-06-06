bde42c99e31f70b4a6ccf912c454a7ce3cb957d1
Arne Schwabe <arne@rfc2549.org>
2023-06-06 12:56:27 +0200

    Use Java 17 instead of Java 8

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -126,6 +126,8 @@ android {
     compileOptions {
         targetCompatibility = JavaVersion.VERSION_1_8
         sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
     }
 
     splits {

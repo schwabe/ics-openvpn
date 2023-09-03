bb5a884b239e7c73380ae2dc40d21c201fd25fd0
conorsmith <conor.smith42@gmail.com>
2023-09-03 16:40:59 +0200

    Use gradle catalog to align project dependencies across modules/gradle files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,22 @@
  * Copyright (c) 2012-2016 Arne Schwabe
  * Distributed under the GNU GPL v2 with additional terms. For full terms see the file doc/LICENSE.txt
  */
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
 
 include(":main")
 include(":tlsexternalcertprovider")

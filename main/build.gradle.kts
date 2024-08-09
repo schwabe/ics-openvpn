e748956722b556d77425af41b53dc73efc1da915
Arne Schwabe <arne@rfc2549.org>
2024-08-09 17:13:41 +0200

    Version 0.7.53

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         minSdk = 21
         targetSdk = 35
         //targetSdkPreview = "UpsideDownCake"
-        versionCode = 207
-        versionName = "0.7.52"
+        versionCode = 208
+        versionName = "0.7.53"
         externalNativeBuild {
             cmake {
                 //arguments+= "-DCMAKE_VERBOSE_MAKEFILE=1"
